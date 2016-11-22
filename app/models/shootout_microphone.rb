c3f3f059ba161ffa9fce658d718ae5773e23d711
ckc1230 <chriskchan1230@gmail.com>
2016-11-22 10:59:20 -0800

    adds audio to new shootout - but just 1 track for now

--- app/models/shootout_microphone.rb
+++ app/models/shootout_microphone.rb
@@ -2,6 +2,5 @@ class ShootoutMicrophone < ApplicationRecord
   belongs_to :shootout, inverse_of: :shootout_microphones
   belongs_to :microphone
   has_attached_file :audio, optional: true
-  validates_attachment_content_type :audio, :content_type => [ 'audio/wav', 'audio/mpeg']
-
+  validates_attachment_content_type :audio, :content_type => [ 'audio/wav', 'audio/mpeg', 'audio/mp3']
 end
