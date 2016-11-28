5fc35bfe4a573331873d3e2b816c8ca8d7035c47
ckc1230 <chriskchan1230@gmail.com>
2016-11-28 10:06:16 -0800

    updates user show page to display which instruments and mics the user has shootouts for

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
+		@instruments = @user.shootouts.each.map(&:instrument).uniq.sort_by &:name
+		shootout_mics = @user.shootouts.each.map(&:shootout_microphones).flatten
+		@microphones = shootout_mics.each.map(&:microphone).uniq.sort_by &:manufacturer
 	end
 end
