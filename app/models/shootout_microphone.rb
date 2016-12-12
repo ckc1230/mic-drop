f013170416ef37076bc26aa7156eb965c89f8c50
ckc1230 <chriskchan1230@gmail.com>
2016-12-12 11:19:41 -0800

    add audio file size validation to 3mb

--- app/models/shootout_microphone.rb
+++ app/models/shootout_microphone.rb
@@ -2,5 +2,6 @@ class ShootoutMicrophone < ApplicationRecord
   belongs_to :shootout, inverse_of: :shootout_microphones
   belongs_to :microphone
   has_attached_file :audio
+  validates_attachment_size :audio, :less_than => 3.megabytes
   validates_attachment_content_type :audio, :content_type => [ 'audio/wav', 'audio/mpeg', 'audio/mp3']
 end
